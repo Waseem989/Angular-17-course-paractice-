e570895fa8dc793ca640d62c51b8ae19dea8337c
waseem989 <kashmiri6980@gmail.com>
2025-08-01 16:22:11 +0500

    single ts file mai html or css likhen ka tareeka agr html code chota hu to

--- creating-component/src/app/profile1.ts
+++ creating-component/src/app/profile1.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
  standalone: true,
  selector:'app-profile1',
- template: '<h1>Hello this text is from profile1 component</h1>',
+ template: '<h1>Hello this text is from single ts file named profile1 component</h1>',
  styles:['h1{color:green;background-color:yellow;}']
 
 })
