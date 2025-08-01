acb1f7aea417597f09db8a4dd0319b615032e813
waseem989 <kashmiri6980@gmail.com>
2025-08-01 18:47:01 +0500

    selecter1 is deafult method to render data

--- creating-component/src/app/components/component-selecters/component-selecters.ts
+++ creating-component/src/app/components/component-selecters/component-selecters.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Selecter2 } from '../selecter2/selecter2';
 
 @Component({
-  selector: 'app-component-selecters',
-  imports: [],
+  selector: 'app-component-selecters,',
+  imports: [Selecter2],
   templateUrl: './component-selecters.html',
   styleUrl: './component-selecters.css'
 })
--- creating-component/src/app/components/selecter2/selecter2.ts
+++ creating-component/src/app/components/selecter2/selecter2.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-selecter2',
+  selector: '[app-selecter2]',
   imports: [],
   templateUrl: './selecter2.html',
   styleUrl: './selecter2.css'
