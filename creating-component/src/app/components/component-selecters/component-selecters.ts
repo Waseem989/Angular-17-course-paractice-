767c6313481eb10234a337593a5ac0fd9f0e39e2
waseem989 <kashmiri6980@gmail.com>
2025-08-05 14:59:03 +0500

    selecter types done

--- creating-component/src/app/components/component-selecters/component-selecters.ts
+++ creating-component/src/app/components/component-selecters/component-selecters.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Selecter2 } from '../selecter2/selecter2';
+import { Component1 } from "../component1/component1";
+import { Selecter1 } from "../selecter1/selecter1";
 
 @Component({
   selector: 'app-component-selecters,',
-  imports: [Selecter2],
+  imports: [Selecter2, Selecter1 ],
   templateUrl: './component-selecters.html',
   styleUrl: './component-selecters.css'
 })
